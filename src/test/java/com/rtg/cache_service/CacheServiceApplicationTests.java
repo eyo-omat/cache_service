567687e1ffd692f9845b59223de24b7e84b1f23a
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-03-26 22:25:07 +0000

    :clown_face: initial commit

---
+++ src/test/java/com/rtg/cache_service/CacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtg.cache_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
